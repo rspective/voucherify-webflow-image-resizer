47cfea442c5004d5dc0df93581e9f58e2138dd41
Piotr Gacek <pgacek0@gmail.com>
2020-06-02 10:46:04 +0200

    Updated readme

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # voucherify-webflow-image-resizer
 Resize Webflow images through Webflow API
 
+This script downloads your images from your specified Webflow Collection, manipulates them and then sends them back.
 
-Use NGROK to host images
+Use `http-server` to start local server inside app folder
+
+Use `ngrok` (https://dashboard.ngrok.com/get-started/setup) to start tunneled connection. Make sure that the local server and Ngrok are using the same port.
+
+Issues:
+
+When updating an Webflow item remember to update also the requried fields (even if you're not changing them)
--- app.js
+++ app.js
@@ -2,37 +2,39 @@ const Webflow = require("webflow-api");
 const download = require("image-downloader");
 const sharp = require("sharp");
 
-
-//Use NGROK to host resized images on localhost
+//Set up local server using http-server in the app folder and in the second terminal tab start Ngrok. Point Ngrok to the same port as local server.
+const ngrok_url = "NGROK_URL"
 
 const api_token =
-  "API_TOKEN";
+  "WEBFLOW_API_TOKEN";
 
-const collectionId = "COLLECTION_ID";
+const collectionId = "WEBFLOW_COLLECTION_ID";
 
 const webflow = new Webflow({ token: api_token });
 
 async function changeImages() {
+  //Connect to your Webflow collection
   const items = webflow.items({ collectionId: collectionId });
 
   images = await items;
 
   for (let i = 0; i < images.items.length; i++) {
+    //Set up the absolute URL to thumbnail image for image download
     let options = {
       url: `${images.items[i]["thumbnail-image"]["url"]}`,
-      dest: "./images/",
+      dest: "./",
     };
 
-    //Download images
+    //Start downloading images
     download
       .image(options)
-      //Resize image
+      //Start resizing images
       .then(({ filename }) => {
-        sharp(filename).toFile(`./images/resized_${i}.jpg`, function (err) {});
+        sharp(filename).toFile(`./resized_${i}.jpg`, function (err) {});
       })
       .catch((err) => console.error(err));
 
-    //Update image on Webflow
+    //Send images to your Webflow Collection through NGROK
     const updateImage = webflow.updateItem({
       collectionId: collectionId,
       itemId: images.items[i]["_id"],
@@ -41,8 +43,8 @@ async function changeImages() {
         slug: images.items[i]["slug"],
         _archived: false,
         _draft: false,
-        "author-2": images.items[i]["author-2"],
-        "thumbnail-image": `http://ec463da7a7fb.ngrok.io/resized_${i}.jpg`,
+        "author-2": images.items[i]["author-2"], //This field may be not important for your configuration
+        "thumbnail-image": `${ngrok_url}resized_${i}.jpg`, //New image URL made with NGROK
       },
     });
 
